1e016d0318b4429b8dcfe24da2579320d6038627
Subrata Panja <37834555+sbtpanja@users.noreply.github.com>
2022-06-14 21:15:30 +0530

    scripts changes

--- script.js
+++ script.js
@@ -3,4 +3,7 @@
             $("#password").addClass("form-control form-control-lg mb-3");
             $("#next").addClass("btn btn-lg btn-success").text("LOGIN");
             $("#next").parent().addClass("d-grid");
+  console.log("script running");
         });
+
+console.log("script loaded");
