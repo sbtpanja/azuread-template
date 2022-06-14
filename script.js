e991127a81a592c20e717986a15e8d12c73114de
Subrata Panja <37834555+sbtpanja@users.noreply.github.com>
2022-06-14 21:04:34 +0530

    create scripts.js
    create scripts.js

---
+++ script.js
@@ -0,0 +1,6 @@
+ $(document).ready(function () {
+            $("#email").addClass("form-control form-control-lg mb-3");
+            $("#password").addClass("form-control form-control-lg mb-3");
+            $("#next").addClass("btn btn-lg btn-success").text("LOGIN");
+            $("#next").parent().addClass("d-grid");
+        });
